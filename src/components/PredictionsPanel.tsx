f8437b9b05d67959d06d7210be9da606ad11e097
kukiel <kukieleq@gmail.com>
2018-03-31 00:26:46 +0200

    adding loading spinner

--- src/components/PredictionsPanel.tsx
+++ src/components/PredictionsPanel.tsx
@@ -1,21 +1,24 @@
 import * as React from 'react';
 
-export const PredictionPanel = ({predicted, predictions = []}) => {
-  return predictions.length ? <div>
-    <h3>It's {predicted} ({predictions[predicted]}%)</h3>
-    <table>
-      <tbody>
-      <tr>
-        {predictions.map((prediction, i) =>
-          <td key={i}>{i}</td>
-        )}
-      </tr>
-      <tr>
-        {predictions.map((prediction, i) =>
-          <td key={i}>{prediction}%</td>
-        )}
-      </tr>
-      </tbody>
-    </table>
-  </div> : null;
+export const PredictionPanel = ({predicted, predictions = [], fetching}) => {
+  return <div>
+    {fetching ? <span className="loader"></span> : undefined}
+    {predictions.length && !fetching ? <div>
+      <h3>It's {predicted} ({predictions[predicted]}%)</h3>
+      <table>
+        <tbody>
+        <tr>
+          {predictions.map((prediction, i) =>
+            <td key={i}>{i}</td>
+          )}
+        </tr>
+        <tr>
+          {predictions.map((prediction, i) =>
+            <td key={i}>{prediction}%</td>
+          )}
+        </tr>
+        </tbody>
+      </table>
+    </div> : null}
+  </div>
 };
--- src/index.tsx
+++ src/index.tsx
@@ -9,19 +9,29 @@ const canvasElement = document.querySelector('.canvas') as HTMLCanvasElement;
 const canvas = new Canvas(canvasElement, 28, 28, 3);
 let predictions = undefined;
 let predicted = undefined;
+let fetching = false;
 
-canvas.updatePrediction$.subscribe((data: any) => {
-  predictions = data.predictions;
-  predicted = data.predicted;
-  renderDOM();
-});
+canvas.updatePrediction$.subscribe(onNewPrediction);
 
 const Container = () => {
   return <div>
-    <ActionsPanel clear={clear} predict={type => canvas.predict(type)}/>
-    <PredictionPanel predicted={predicted} predictions={predictions}/>
+    <ActionsPanel clear={clear} predict={getPrediction}/>
+    <PredictionPanel fetching={fetching} predicted={predicted} predictions={predictions}/>
   </div>
 };
+
+function getPrediction(type) {
+  fetching = true;
+  canvas.predict(type);
+  renderDOM();
+}
+
+function onNewPrediction(data: any) {
+  predictions = data.predictions;
+  predicted = data.predicted;
+  fetching = false;
+  renderDOM();
+}
 
 function clear() {
   canvas.clearCanvas();
